5c744401199c05427b9b1fa3507572a2d6556c5e
MerttBodur <mert.bodur@bahcesehir.edu.tr>
2026-04-19 17:07:38 +0300

    chore: add CommunityToolkit.Maui package for popup support

--- CodeBehind/MauiProgram.cs
+++ CodeBehind/MauiProgram.cs
@@ -13,6 +13,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
 			.UseMauiCommunityToolkitMediaElement(false)
 			.ConfigureFonts(fonts =>
 			{
