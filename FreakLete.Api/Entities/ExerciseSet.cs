9ede25b99de3badcdb3f370640eaac762faea948
MerttBodur <mert.bodur@bahcesehir.edu.tr>
2026-04-24 23:45:48 +0300

    feat: add RIR/Rest/Concentric fields to ExerciseSet entity

--- FreakLete.Api/Entities/ExerciseSet.cs
+++ FreakLete.Api/Entities/ExerciseSet.cs
@@ -7,6 +7,9 @@ public class ExerciseSet
     public int SetNumber { get; set; }
     public int Reps { get; set; }
     public double? Weight { get; set; }
+    public int? RIR { get; set; }
+    public int? RestSeconds { get; set; }
+    public double? ConcentricTimeSeconds { get; set; }
 
     public ExerciseEntry ExerciseEntry { get; set; } = null!;
 }
