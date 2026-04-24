28110a3ee49e10af6e7c6cf86cd54aad1e51fe67
MerttBodur <mert.bodur@bahcesehir.edu.tr>
2026-04-24 23:47:00 +0300

    feat: extend ExerciseSetDto with RIR/Rest/Concentric

--- FreakLete.Api/DTOs/Workout/ExerciseSetDto.cs
+++ FreakLete.Api/DTOs/Workout/ExerciseSetDto.cs
@@ -5,4 +5,7 @@ public class ExerciseSetDto
     public int SetNumber { get; set; }
     public int Reps { get; set; }
     public double? Weight { get; set; }
+    public int? RIR { get; set; }
+    public int? RestSeconds { get; set; }
+    public double? ConcentricTimeSeconds { get; set; }
 }
