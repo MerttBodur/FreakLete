9dcb2f941e0eb5383590cd68832572501eb73ab0
MerttBodur <mert.bodur@bahcesehir.edu.tr>
2026-04-24 12:30:35 +0300

    feat: add SetDetail client model

---
+++ Models/SetDetail.cs
@@ -0,0 +1,8 @@
+namespace FreakLete.Models;
+
+public sealed class SetDetail
+{
+    public int SetNumber { get; init; }
+    public int Reps { get; set; }
+    public double? Weight { get; set; }
+}
